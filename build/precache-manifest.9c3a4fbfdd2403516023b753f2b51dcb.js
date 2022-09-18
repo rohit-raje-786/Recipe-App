8fd6294c6fa3642e4857b9cee8ea65c3336b05ba
Rohit Patil <rp589006@gmail.com>
2022-09-18 07:49:18 +0000

    build

---
+++ build/precache-manifest.9c3a4fbfdd2403516023b753f2b51dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2a6ebcb7cc2fecfc063ca397d04855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4daff5b56ef35c54f39e",
+    "url": "/static/css/main.72d43089.chunk.css"
+  },
+  {
+    "revision": "c380c62415fd2d1d2014",
+    "url": "/static/js/2.ccae311e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ccae311e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4daff5b56ef35c54f39e",
+    "url": "/static/js/main.2f6bc9b6.chunk.js"
+  },
+  {
+    "revision": "afef85084f0804340619",
+    "url": "/static/js/runtime-main.2b370e64.js"
+  }
+]);
